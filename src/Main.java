7e5200b1b5795e8cb51a7ca27e95e6715b930695
Kadambari-Ozarkar <kadambari0809@gmail.com>
2026-07-26 19:36:33 +0530

    java upd2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+```java
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -7,12 +8,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        // Create HTTP server on port 8080
         HttpServer server = HttpServer.create(
                 new InetSocketAddress(8080), 0
         );
 
-        // Handle requests
         server.createContext("/", exchange -> {
 
             String response = "CI/CD Security Project is running!";
@@ -27,12 +26,16 @@ public class Main {
             }
         });
 
-        // Start server
         server.start();
 
         System.out.println("Application running on port 8080");
 
-        // Keep Java process alive
-        Thread.currentThread().join();
+        // Keep application running
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
+```
