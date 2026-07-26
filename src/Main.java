aa1069f0ac477ed0c53ac52279ba8f53b6e3bf20
Kadambari-Ozarkar <kadambari0809@gmail.com>
2026-07-26 19:39:26 +0530

    upd3

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,42 @@
-```java
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+```
+public static void main(String[] args) throws IOException {
 
-        HttpServer server = HttpServer.create(
-                new InetSocketAddress(8080), 0
+    HttpServer server = HttpServer.create(
+            new InetSocketAddress(8080), 0
+    );
+
+    server.createContext("/", (HttpExchange exchange) -> {
+
+        String response = "CI/CD Security Project is running!";
+
+        exchange.getResponseHeaders().set(
+                "Content-Type",
+                "text/plain"
         );
 
-        server.createContext("/", exchange -> {
+        exchange.sendResponseHeaders(
+                200,
+                response.getBytes().length
+        );
 
-            String response = "CI/CD Security Project is running!";
-
-            exchange.sendResponseHeaders(
-                    200,
-                    response.getBytes().length
-            );
-
-            try (OutputStream output = exchange.getResponseBody()) {
-                output.write(response.getBytes());
-            }
-        });
-
-        server.start();
-
-        System.out.println("Application running on port 8080");
-
-        // Keep application running
-        try {
-            Thread.currentThread().join();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        try (OutputStream output = exchange.getResponseBody()) {
+            output.write(response.getBytes());
         }
-    }
+    });
+
+    server.start();
+
+    System.out.println("Application running on port 8080");
 }
 ```
+
+}
